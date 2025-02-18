655b261b04bf99c2797efa68dd441a3682777eff
Ayush <ayush28.connect@gmail.com>
2025-02-19 02:57:54 +0530

    script updated

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,6 +3,4 @@
 cd ~/ExamWebApp/frontend/ && npm install
 cd ~/ExamWebApp/backend/ && npm install
 
-cd ~/ExamWebApp && pm2 kill
-cd ~/ExamWebApp/backend/ && pm2 start --name "backend1" app.js
-cd ~/ExamWebApp/frontend/ && pm2 start npm --name  "frontend1" -- run dev
+cd ~/ExamWebApp && sudo systemctl restart pm2-ubuntu.service
